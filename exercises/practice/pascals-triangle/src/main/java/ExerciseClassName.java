78a34b01894f23522c116b97fbd082d05dd6bb15
boomak <46297167+boomak@users.noreply.github.com>
2022-04-28 17:23:39 +0530

    Create ExerciseClassName.java

---
+++ exercises/practice/pascals-triangle/src/main/java/ExerciseClassName.java
@@ -0,0 +1,38 @@
+public class ExerciseClassName
+{
+
+    int rows;
+
+    ExerciseClassName(int rows)
+    {
+        this.rows=rows;
+    }
+
+
+    int[][] generateTriangle()
+    {
+        int[][] arr= new int[rows][rows];
+        int coe=1;
+
+        for(int i = 0; i < rows; i++)
+        {
+
+            for(int j = 0; j <= i; j++)
+            {
+                if (j == 0 || i == 0)
+                    coe = 1;
+                else
+                    coe = coe * (i - j + 1) / j;
+                arr[i][j]=coe;
+
+
+            }
+
+        }
+        return arr;
+    }
+
+}
+
+
+
